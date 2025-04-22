fb969144f9d640babd9ea8858ce740eb8a442f7d
Nicole Martins <nicolemartins@alunos.utfpr.edu.br>
2025-04-21 22:51:43 -0300

    feat: bookshelf page (unauth view)

--- lib/pages/estante_page.dart
+++ lib/pages/estante_page.dart
@@ -10,6 +10,50 @@ class BookshelfPage extends StatefulWidget {
 class _BookshelfPageState extends State<BookshelfPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Container(color: Colors.grey[50]));
+    return Column(
+      children: [
+        Padding(
+          padding: EdgeInsets.fromLTRB(5, 200, 5, 25),
+          child: Center(
+            child: Text(
+              'Acesse sua conta para visualizar sua estante',
+              style: TextStyle(fontSize: 20),
+              textAlign: TextAlign.center,
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: ElevatedButton(
+            onPressed: () {},
+            style: ElevatedButton.styleFrom(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(8),
+              ),
+              backgroundColor: Colors.blueGrey[900],
+              padding: EdgeInsets.fromLTRB(20, 12, 20, 12),
+            ),
+            child: Text(
+              'LOGIN',
+              style: TextStyle(color: Colors.white, fontSize: 16),
+            ),
+          ),
+        ),
+        ElevatedButton(
+          onPressed: () {},
+          style: ElevatedButton.styleFrom(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(8),
+            ),
+            backgroundColor: Colors.blueGrey[900],
+            padding: EdgeInsets.fromLTRB(20, 12, 20, 12),
+          ),
+          child: Text(
+            'CADASTRAR',
+            style: TextStyle(color: Colors.white, fontSize: 16),
+          ),
+        ),
+      ],
+    );
   }
 }
